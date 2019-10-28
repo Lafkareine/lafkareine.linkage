bd78c736cb3955deef38534aa85bb9c22eca6e84
Lafkareine <tendonsksk@gmail.com>
2019-10-28 16:18:44 +0900

    exam

--- src/lafkareine/util/linkage/LinkableListPath.java
+++ src/lafkareine/util/linkage/LinkableListPath.java
@@ -10,11 +10,11 @@ public class LinkableListPath<T, U extends LinkableBase> extends LinkableBase {
 		U get(T from);
 	}
 
-	public interface Listener<U extends LinkableBase> {
-		void exchange (U from, U to);
+	public interface Listener{
+		void exchange ();
 	}
 
-	private Listener<U>[] listeners = NOTHING_LISTENER;
+	private Listener[] listeners = NOTHING_LISTENER;
 
 	private static final Listener[] NOTHING_LISTENER = new Listener[] {};
 
@@ -30,32 +30,43 @@ public class LinkableListPath<T, U extends LinkableBase> extends LinkableBase {
 	public void set(ReadOnlyLinkable<List<T>> from, Getter<T,U> getter) {
 		this.from = from;
 		this.getter = getter;
-		launchUpdate(makeIputsArray(from, getter));}
+		launchUpdate(makeInputsArray(from, getter));}
 
 	public void set(ReadOnlyLinkable<List<T>> from) {
 		this.from = from;
-		launchUpdate(makeIputsArray(from, getter));}
+		launchUpdate(makeInputsArray(from, getter));
+	}
+
+	public final U pick(int index){
+		return getter.get(from.get().get(index));
+	}
 
 	@Override
 	protected void action() {
 		// TODO 自動生成されたメソッド・スタブ
-		setInputsInSecretly(makeIputsArray(from, getter));
+		setInputsInSecretly(makeInputsArray(from, getter));
 	}
 
-	private static <T, U extends LinkableBase> LinkableBase[] makeIputsArray(ReadOnlyLinkable<List<T>> from, Getter<T,U> getter){
+	private void runListener(){
+		for(var e:listeners){
+			e.exchange();
+		}
+	}
+
+	private static <T, U extends LinkableBase> LinkableBase[] makeInputsArray(ReadOnlyLinkable<List<T>> from, Getter<T,U> getter){
 		LinkableBase[] parents = new LinkableBase[from.get().size()+1];
 		int i = 0;
 		for(var e:from.get()){
 			parents[i++] = getter.get(e);
 		}
 		parents[i] = from;
-
+		return  parents;
 	}
 
-	public final Listener<U> addListner(Listener<U> added){
+	public final Listener addListner(Listener added){
 		if(added == null) return null;
-		final Listener<U>[] new_array = new Listener[listeners.length+1];
-		final Listener<U>[] old_array = listeners;
+		final Listener[] new_array = new Listener[listeners.length+1];
+		final Listener[] old_array = listeners;
 		for(int i = 0; i < old_array.length; i++){
 			new_array[i] = old_array[i];
 		}
@@ -63,14 +74,14 @@ public class LinkableListPath<T, U extends LinkableBase> extends LinkableBase {
 		return  added;
 	}
 
-	public final boolean removeListner(Listener<U> added){
-		final Listener<U>[] old_array = listeners;
+	public final boolean removeListner(Listener added){
+		final Listener[] old_array = listeners;
 		int nullnum = 0;
 		for(int i = 0; i < old_array.length; i++){
 			if(old_array[i] == added)old_array[i] = null;
 			nullnum += 1;
 		}
-		final Listener<U>[] new_array = new Listener[listeners.length-nullnum];
+		final Listener[] new_array = new Listener[listeners.length-nullnum];
 		int index_to = 0;
 		for(var e:old_array){
 			if(e != null)new_array[index_to++] = e;
